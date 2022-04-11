a90729c133ba8b54f21063db3514f6534c887975
Zachary Kestner <zachary.kestner@student.awesomeinc.org>
2022-04-11 20:03:45 +0000

    added models for three fields and migrated changes to database

--- music/models.py
+++ music/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Song(models.Model):
+    song_name = models.CharField(max_length=500)
+
+class Artist(models.Model):
+    artist_name = models.CharField(max_length=500)
+
+class Album(models.Model):
+    song = models.ManyToManyField(Song)
+    artist = models.ManyToManyField(Artist)
+    album_name = models.CharField(max_length=500)
