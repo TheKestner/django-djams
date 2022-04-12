f23f4267b83543cd3944124e69e848420af8348f
Zachary Kestner <zachary.kestner@student.awesomeinc.org>
2022-04-12 17:05:44 +0000

    added str to models

--- music/models.py
+++ music/models.py
@@ -5,10 +5,19 @@ from django.db import models
 class Song(models.Model):
     song_name = models.CharField(max_length=500)
 
+    def __str__(self):
+        return self.song_name
+
 class Artist(models.Model):
     artist_name = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.artist_name
 
 class Album(models.Model):
     song = models.ManyToManyField(Song)
     artist = models.ManyToManyField(Artist)
     album_name = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.album_name
