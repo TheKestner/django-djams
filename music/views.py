a98778c3ee44f2d922bace1bd7cf28a7a50021cb
Zachary Kestner <zachary.kestner@student.awesomeinc.org>
2022-04-12 18:50:39 +0000

    saving progress with making json views

--- music/views.py
+++ music/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
-
+from django.http import HttpResponse, JsonResponse
+from .models import *
 
 def index(request):
     return HttpResponse("Hello, world. You're at the music index.")
+
+def results(request):
+    data = {}
+    songs = Song.objects.all().values()
+    data = list(songs) 
+    return JsonResponse({"Data":data})
